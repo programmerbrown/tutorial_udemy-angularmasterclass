fe71a56390631dbdf3def73e871e84b3fc7884f1
Nick Brown <nicholas.g.brown@gmail.com>
2017-08-21 18:52:48 -0400

    Completed lesson 33

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,8 +2,16 @@ import{ Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+    <h2>{{ title }}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+            {{ course }}
+        </li>
+    </ul>
+    `
 })
 export class CoursesComponent {
-
+    title = "List of courses";
+    courses = ["course1", "course2", "course3"];
 }
