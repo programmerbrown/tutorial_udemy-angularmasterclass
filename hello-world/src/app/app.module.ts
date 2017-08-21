98b8be1c3d7c334d6bfb762000bf9a70ccc4fd1d
Nick Brown <nicholas.g.brown@gmail.com>
2017-08-21 18:34:25 -0400

    Completed lesson 30.

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import{ Component } from '@angular/core';
+
+@Component({
+    selector: 'courses',
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+
+}
