ecded46c783dbd1edda93331e6816203647e9657
Nick Brown <nicholas.g.brown@gmail.com>
2017-08-27 12:38:37 -0400

    (1) Fixed bug (2) Refactored the code (3) Added comments

--- assignment3/src/app/title.pipe.ts
+++ assignment3/src/app/title.pipe.ts
@@ -5,16 +5,35 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitlePipe implements PipeTransform {
     transform(value: string): string {
-        let reToReplaceOf = /(\sof\s|\sof$)/gi;
-        let reToReplaceThe = /(\sthe\s|\sthe$)/gi;
-        let reToReplaceAnd = /(\sand\s|\sand$)/gi;
-
+        /*
+        * A list of words that I want to be lower case in the 
+        * title unless they are the first word.
+        */
+        let wordList = [
+            'of',
+            'the',
+            'and',
+            'to'
+        ];
+        
         if(!value)
-            return "";
-        else
-            return value.replace(reToReplaceOf, " of")
-                         .replace(reToReplaceThe, " the")
-                         .replace(reToReplaceAnd, " and");
+            return "";  // if the value is null return an empty string
+        else {
+            /*
+            * Goal: Walk through the list of words
+            * generating a regular expression and replace
+            * any matches with that word preceded by a whitespace.
+            * Notes:
+            * - Ex regular expression for the word 'of': /\sof(?=\s|$)/gi
+            * - the preceding whitespace character lets us know if the
+            *   word is the first word in the title or not. 
+            */
+            wordList.forEach(function(word) {
+                let re = '\\s' + word + '(?=\\s|$)';
+                let regExp = new RegExp(re, "gi");
+                value = value.replace(regExp, " "+word);
+            });
+            return value;
+        }
     }
-    
 }
