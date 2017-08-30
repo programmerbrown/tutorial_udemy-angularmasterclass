d7ba5b962c8abfcd8d189564d9785ae4fec4e9a1
Nick Brown <nicholas.g.brown@gmail.com>
2017-08-30 19:49:08 -0400

    Switched assignment 4 to use bootstrap instead of Angular material

--- assignment4/src/app/app.module.ts
+++ assignment4/src/app/app.module.ts
@@ -1,9 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MdIconRegistry } from '@angular/material';
-import { MdIconModule } from '@angular/material';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FavoriteComponent } from './favorite.component';
@@ -15,15 +11,9 @@ import { FavoriteComponent } from './favorite.component';
     FavoriteComponent
   ],
   imports: [
-    BrowserModule,
-    HttpModule,
-    MdIconModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-//   constructor(private mdIconRegistry: MdIconRegistry, private domSanitizer: DomSanitizer){
-//     mdIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('/assets/favorite_border.svg'));
-// }
-}
+export class AppModule { }
--- assignment4/src/app/favorite.component.ts
+++ assignment4/src/app/favorite.component.ts
@@ -1,31 +1,16 @@
 import { Component } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MdIconRegistry } from '@angular/material';
-import { MdIconModule } from '@angular/material';
-
 
 @Component({
     selector: 'favorite',
     template:`
-    <button (click)="toggleIcon()" style="border: none; background-color: white;">
-        <md-icon aria-hidden="true" [svgIcon]="iconState"></md-icon> 
-    </button> 
-    <div>{{ numOfLikes }}</div>   
+    <span class="glyphicon" (click)="toggleIcon()" [class.glyphicon-star]="isActive" [class.glyphicon-star-empty]="!isActive" aria-hidden="true">{{ numOfLikes }}</span>  
     `
 })
 export class FavoriteComponent {
     numOfLikes: number = 10;
-    iconState = "favorite_border";
-
-    constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
-        iconRegistry.addSvgIcon('favorite_border', sanitizer.bypassSecurityTrustResourceUrl('assets/favorite_border.svg'));
-        iconRegistry.addSvgIcon('favorite', sanitizer.bypassSecurityTrustResourceUrl('assets/favorite.svg'))
-      }
+    isActive: boolean = false;
 
     toggleIcon() {
-        if(this.iconState == "favorite_border")
-            this.iconState = "favorite";
-        else
-            this.iconState = "favorite_border";
+        this.isActive = !this.isActive;
     }
 }
