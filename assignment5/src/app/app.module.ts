82ad5c16362d1ca154ac4ad88fb17cefc1e89831
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-02 10:36:45 -0400

    Added zippy chevron glyphicon, html and css for zippy component, and a method to toggle the chevron glyphicon

--- assignment5/src/app/app.module.ts
+++ assignment5/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ZippyComponent } from './zippy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZippyComponent
   ],
   imports: [
     BrowserModule
---
+++ assignment5/src/app/zippy.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+    selector: 'zippy',
+    templateUrl: './zippy.component.html',
+    styleUrls: ['./zippy.component.css']
+})
+export class ZippyComponent {
+    //@Input() title: string;
+    isOpen: boolean = false;
+    
+    toggle() {
+        this.isOpen = !this.isOpen;
+    }
+}
