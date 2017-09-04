a89b19870e2eaa63a17f44aef30fc5b03c704be2
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-04 15:34:15 -0400

    Completed lesson 88

--- hello-world/src/app/contact-form/contact-form.component.ts
+++ hello-world/src/app/contact-form/contact-form.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
+  contactMethods = [
+    {id: 1, name: 'Email'},
+    {id: 2, name: 'Phone'}
+  ];
+
   log(x) {
     console.log(x);
   }
