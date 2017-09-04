59507565c634b5b85707e76fc71460a19e7f56e3
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-04 13:31:00 -0400

    Completed lesson 78

--- hello-world/src/app/contact-form/contact-form.component.ts
+++ hello-world/src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ContactFormComponent {
+  log(x) {
+    console.log(x);
   }
-
 }
