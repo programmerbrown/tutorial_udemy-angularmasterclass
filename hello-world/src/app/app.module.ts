8d9469a8c90b8269cfb4a2d2be1b058975b55d84
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-05 18:47:42 -0400

    Completed lesson 91

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent         } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent  } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupFormComponent,
     CoursesComponent,
     ContactFormComponent
   ],
