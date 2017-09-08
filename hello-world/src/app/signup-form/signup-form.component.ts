d03d22b947c137e1d262d56baa81ad77e236c800
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-08 17:43:04 -0400

    Completed lesson 100

--- hello-world/src/app/signup-form/signup-form.component.ts
+++ hello-world/src/app/signup-form/signup-form.component.ts
@@ -11,13 +11,16 @@ import { UsernameValidators } from './username.validators';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace,
-    ], UsernameValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        UsernameValidators.cannotContainSpace,
+      ], UsernameValidators.shouldBeUnique),
+      password: new FormControl('', Validators.required)
+    })
   });
+
 
   login() {
     this.form.setErrors({
@@ -26,6 +29,6 @@ export class SignupFormComponent {
   }
 
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
