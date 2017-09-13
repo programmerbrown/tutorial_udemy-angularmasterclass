be7abd389ccd9fd87534a2cb916983bbab089784
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-13 19:53:04 -0400

    Complete lesson 118

--- section9/src/app/services/post.service.ts
+++ section9/src/app/services/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 import { AppError } from '.././common/app-error';
 import { NotFoundError } from '.././common/not-found-error';
 import { BadInput } from '.././common/bad-input';
