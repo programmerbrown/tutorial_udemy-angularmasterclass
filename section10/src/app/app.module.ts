2f8166ebcfa20689fa6903cddecde826ea5b0c1a
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-18 18:06:35 -0400

    Completed lesson 133

--- section10/src/app/app.module.ts
+++ section10/src/app/app.module.ts
@@ -66,7 +66,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
         component: HomeComponent 
       },
       { 
-        path: 'followers/:id', 
+        path: 'followers/:id/:username', 
         component: GithubProfileComponent 
       },
       { 
