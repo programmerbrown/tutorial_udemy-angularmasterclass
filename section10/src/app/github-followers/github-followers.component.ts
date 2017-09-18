092934e10d60bcaf8c937d268ad461b088da8f53
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-18 18:54:54 -0400

    Completed lesson 136

--- section10/src/app/github-followers/github-followers.component.ts
+++ section10/src/app/github-followers/github-followers.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'github-followers',
@@ -20,13 +22,12 @@ export class GithubFollowersComponent implements OnInit {
     Observable.combineLatest([
       this.route.paramMap,
       this.route.queryParamMap
-    ]).subscribe(combined => {
+    ]).switchMap(combined => {
       let id = combined[0].get('id');
       let page = combined[1].get('page');
 
-      this.service.getAll()
-                  .subscribe(followers => this.followers = followers);
-    });
+      return this.service.getAll();
+    }).subscribe(followers => this.followers = followers);
 
 
   }
