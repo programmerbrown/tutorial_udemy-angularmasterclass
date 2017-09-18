5d67321c73eee12e719ee6d733c55fd0fcb4301c
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-18 19:39:48 -0400

    Completed lesson 137

--- section10/src/app/github-profile/github-profile.component.ts
+++ section10/src/app/github-profile/github-profile.component.ts
@@ -1,20 +1,19 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
-export class GithubProfileComponent implements OnInit {
+export class GithubProfileComponent {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private router: Router ) { }
 
-  ngOnInit() {
-    this.route.paramMap
-              .subscribe(params => {
-                let id = +params.get('id');
-                console.log(id);
-              });
-            }
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'oldest' }
+    });
+  }
 }
