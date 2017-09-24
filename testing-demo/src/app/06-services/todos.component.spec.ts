fb43d3d5a4b58dd1d9377a679a5d6719121df33f
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-24 11:41:42 -0400

    Completed lesson 255

--- testing-demo/src/app/06-services/todos.component.spec.ts
+++ testing-demo/src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,27 @@
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
-// describe('TodosComponent', () => {
-//   let component: TodosComponent;
+describe('TodosComponent', () => {
+  let component: TodosComponent;
+  let service: TodoService;
 
-//   beforeEach(() => {
-//   });
+  beforeEach(() => {
+      service = new TodoService(null);
+      component = new TodosComponent(service);
+  });
 
-//   it('', () => {
-//   });
-// });
+  it('should set todos property with the items returned from the server', () => {
+    let todos = [1, 2, 3];  
+    
+    spyOn(service, 'getTodos').and.callFake(() => {
+        return Observable.from([ todos ]);
+      });
+
+      component.ngOnInit();
+
+      expect(component.todos).toBe(todos);
+
+  });
+});
