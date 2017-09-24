3a8bcb30577b76248e07943b87462877a599a590
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-24 10:50:40 -0400

    Completed lesson 253

--- testing-demo/src/app/04-forms/todo-form.component.spec.ts
+++ testing-demo/src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,18 @@
+import { FormBuilder } from '@angular/forms';
 import { TodoFormComponent } from './todo-form.component'; 
 
-// describe('TodoFormComponent', () => {
-//   var component: TodoFormComponent; 
+describe('TodoFormComponent', () => {
+  var component: TodoFormComponent; 
 
-//   beforeEach(() => {
+  beforeEach(() => {
+    component = new TodoFormComponent(new FormBuilder());
+  });
 
-//   });
+  it('it should create a form with 2 controls', () => {
+      expect(component.form.contains('name')).toBe(true);
+      expect(component.form.contains('email')).toBe(true);
+  });
 
-//   it('', () => {
-//   });
-
-//   it('', () => {
-//   });
-// });
+  it('', () => {
+  });
+});
