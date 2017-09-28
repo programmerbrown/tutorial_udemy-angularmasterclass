40d07b7d4306382c52a95fafb7b1a42e22949bc2
Nick Brown <nicholas.g.brown@gmail.com>
2017-09-28 19:46:53 -0400

    Completed lesson 268

--- integration-testing-demo/src/app/3-user-details/user-details.component.spec.ts
+++ integration-testing-demo/src/app/3-user-details/user-details.component.spec.ts
@@ -2,16 +2,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { UserDetailsComponent } from './user-details.component';
 
-xdescribe('UserDetailsComponent', () => {
+class RouterStub {
+  navigate(params) {
+  }
+}
+
+class ActivatedRouteStub {
+  params: Observable<any> = Observable.empty();
+}
+
+describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [ UserDetailsComponent ],
+      providers: [  
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub }
+      ]
     })
     .compileComponents();
   }));
