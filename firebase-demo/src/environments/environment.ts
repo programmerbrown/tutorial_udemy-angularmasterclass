89033efe362bd8fdf340af250a93bb6a7fd716da
Nick Brown <nicholas.g.brown@gmail.com>
2017-10-16 16:48:47 -0400

    Added a project for exploring Firebase

---
+++ firebase-demo/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDaEkqCs3t1Zp2XYuauJmg-SrmioCUxp9Y",
+    authDomain: "fir-demo-2b134.firebaseapp.com",
+    databaseURL: "https://fir-demo-2b134.firebaseio.com",
+    projectId: "fir-demo-2b134",
+    storageBucket: "fir-demo-2b134.appspot.com",
+    messagingSenderId: "255360801928"
+  }
+};
